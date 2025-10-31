217085df3a689c81a296882de589761ed8667811
ismail javed <ismailjaved.dev@gmail.com>
2025-11-01 02:21:12 +0500

    text animation

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import React from 'react'
 import { useEffect, useState } from "react";
 import Lenis from "lenis";
+import { TextAnimate } from '../ui/text-animate';
 
 
 const Hero = () => {
@@ -57,10 +58,10 @@ const Hero = () => {
 
     return (
         <div className='relative overflow-hidden '>
-           <div className='relative pt-[150px] pb-[270px] sm:pb-[350px] md:pb-[450px] lg:pb-[490px] overflow-hidden px-5'>
+           <div className='relative pt-[150px] md:pt-[170px] lg:pt-[200px] pb-[270px] sm:pb-[350px] md:pb-[450px] lg:pb-[490px] overflow-hidden px-5'>
              <Image src={'/heroBg.png'} fill alt='' className='object-cover object-center -z-10 h-[113%]!' />
-            <h1 className='font-bold text-[40px] md:text-[50px] lg:text-[55px] text-center max-w-[650px] mx-auto leading-[1.2]'>Transform Opportunities
-                into Profits</h1>
+             <TextAnimate animation="slideUp" by="word" className={'font-bold text-[40px] md:text-[50px] lg:text-[55px] text-center max-w-[710px] mx-auto leading-[1.2]'}>
+Transform Opportunities into Profits    </TextAnimate>
             <p className='text-lg lg:text-xl text-center max-w-[625px] mx-auto mt-5 '>Trade with the world's largest retail broker and benefit from
                 better-than-market conditions.</p>
            </div>
